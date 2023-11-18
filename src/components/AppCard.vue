cf1b0d8262a2342a1b89ec96ef5984e1126dcc32
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-18 15:39:14 +0100

    Add base layout and connection to api

---
+++ src/components/AppCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'AppCard',
+    props: {
+        'project': Object
+    },
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        
+            <img :src="project.thumb" alt="">
+        
+        <div class="card-body h-100 d-flex flex-column justify-content-around">
+            <h3 class="text-center text-truncate">{{ project.title }}</h3>
+            <div class="d-flex justify-content-center flex-wrap">
+                <small class="badge rounded-pill text-bg-primary m-1" v-for="technology in project.technologies">{{
+                    technology.name }}</small>
+            </div>
+            <div class="d-flex justify-content-around py-1">
+                <a :href="project.website_link" class="btn btn-primary">
+                    Website link
+                </a>
+                <a :href="project.github_link" class="btn btn-dark text-white">
+                    GitHub Link
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: 'AppFooter',
+}
+</script>
+
+<template>
+    <footer id="app_footer" class="bg-dark text-light py-3">
+
+        <div class="container ">
+            <div class="row justify-content-between text-center">
+                <div class="col ">
+                    <ul class="list-unstyled">
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <ul class="list-unstyled">
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <ul class="list-unstyled">
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <!-- /#app_footer -->
+</template>
+
+<style lang="scss" scoped></style>
